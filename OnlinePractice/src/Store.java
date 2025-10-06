3c29e3b79bee5f6751b8afa1210a69778f79e5af
Praise-El Kachamakwara <praiseelkachaz@gmail.com>
2025-10-06 13:17:38 +0200

    Add files via upload

---
+++ OnlinePractice/src/Store.java
@@ -0,0 +1,21 @@
+public class Store {
+    // instance fields
+    String productType;
+
+    // constructor method
+    public Store(String product) {
+        productType = product;
+    }
+
+    // advertise method
+    public void advertise() {
+        String message = "Selling " + productType + "!";
+        System.out.println(message);
+    }
+
+    // greetCustomer() method
+    public void greetCustomer(String customerName){
+        System.out.println("Welcome to the store, " + customerName + "!");
+    }
+
+}
